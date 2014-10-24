71bd598f57a26805d4c30abd394a83fc1af88cd0
tsimonnet <scorpheus.mr@gmail.com>
2014-10-24 09:25:23 +0200

    reduce the decision maker with the new sensors.

--- node_2_inputs_move_right_left_wheel/decision_maker.py
+++ node_2_inputs_move_right_left_wheel/decision_maker.py
@@ -18,17 +18,17 @@ class DecisionMaker(BaseDecisionMaker):
 		if action & Actions.action_right_backward and action & Actions.action_right_forward:
 			return False
 
-		if fragment.input_array[0] > 20 and fragment.input_array[1] > 20 and (action & Actions.action_right_forward or action & Actions.action_left_forward):
+		if fragment.input_array[0] > 2 and fragment.input_array[1] > 2 and (action & Actions.action_right_forward or action & Actions.action_left_forward):
 			return True
-		if fragment.input_array[0] < 20 and fragment.input_array[1] < 20 and (action & Actions.action_right_backward or action & Actions.action_left_backward):
+		if fragment.input_array[0] < 2 and fragment.input_array[1] < 2 and (action & Actions.action_right_backward or action & Actions.action_left_backward):
 			return True
-		if fragment.input_array[0] < 20 < fragment.input_array[1] and (action & Actions.action_right_backward or action & Actions.action_left_forward):
+		if fragment.input_array[0] < 2 < fragment.input_array[1] and (action & Actions.action_right_backward or action & Actions.action_left_forward):
 			return True
-		if fragment.input_array[0] > 20 > fragment.input_array[1] and (action & Actions.action_right_forward or action & Actions.action_left_backward):
+		if fragment.input_array[0] > 2 > fragment.input_array[1] and (action & Actions.action_right_forward or action & Actions.action_left_backward):
 			return True
 
 
-		if fragment.input_array[2] <= 10 and (action & Actions.action_right_forward and action & Actions.action_left_forward):
+		if fragment.input_array[2] <= 1 and (action & Actions.action_right_forward or action & Actions.action_left_forward):
 			return True
 
 		return False
