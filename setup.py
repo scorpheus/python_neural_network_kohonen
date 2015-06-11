379488665e718fa6bb33b0465fb28d8dddcf59ec
tsimonnet <scorpheus.mr@gmail.com>
2015-06-11 12:16:23 +0200

    add build setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+import sys
+from cx_Freeze import setup, Executable
+
+# Dependencies are automatically detected, but it might need fine tuning.
+options = {
+    'build_exe': {
+        'compressed': True,
+		'include_files': ['node_2_inputs_move_right_left_wheel/', 'actions.py', 'decision_maker.py', 'node_inputs.py', 'vector_helper.py']
+    }
+}
+
+setup(  name = "AI",
+        version = "0.1",
+        description = "Kohonen network",
+        options = options,
+        executables = [Executable("main.py")])
